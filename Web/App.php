dfe04b055bb837019016af48b718daaa66a33623
ckwongloy <ckwongloy@gmail.com>
2019-01-09 15:16:57 +0800

    updated

--- Web/App.php
+++ Web/App.php
@@ -8,6 +8,6 @@ class App
 {
     public static function run(string $rootPath)
     {
-        echo 'current project root absolute path is: ', $rootPath;
+        echo 'absolute path of current project root is: ', $rootPath;
     }
 }
