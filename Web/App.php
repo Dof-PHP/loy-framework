b6ee81abd1a6547a602c3c3dd92a8cd17d751a28
ckwongloy <ckwongloy@gmail.com>
2019-01-09 17:22:26 +0800

    updated

--- Web/App.php
+++ Web/App.php
@@ -6,11 +6,4 @@ namespace Loy\Framework\Web;
 
 class App
 {
-    public function errorReporting()
-    {
-        error_reporting(E_ALL);
-
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-    }
 }
--- Web/Kernel.php
+++ Web/Kernel.php
@@ -4,25 +4,35 @@ declare(strict_types=1);
 
 namespace Loy\Framework\Web;
 
+use Exception;
+
+use Loy\Framework\Web\Domain;
+
 class Kernel
 {
     public static function handle(string $projectRootPath)
     {
-        $projectRootPath;
+        self::initErrorReporting();
+
+        if (! is_dir($projectRootPath)) {
+            throw new Exception('Invalid Project Root!');
+        }
+
+        $_SERVER['__LOY_FRAMEWORK_WEB'] = [
+            'project_root' => $projectRootPath,
+        ];
+
+        Domain::compile($projectRootPath);
     }
 
-    public static function getCacheDirectory() : string
+    /**
+     * @TODO: override from config
+     */
+    public static function initErrorReporting()
     {
-        return 'var/cache/web';
-    }
+        error_reporting(E_ALL);
 
-    public static function getLogDirectory() : string
-    {
-        return 'storage/log';
-    }
-
-    public static function getDomainsDirectory() : string
-    {
-        return 'domains';
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
     }
 }
