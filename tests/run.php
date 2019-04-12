0530bde39994a970191e44d6f9064a5881e8edce
ckwongloy <ckwongloy@gmail.com>
2019-04-12 21:29:51 +0800

    updated

--- tests/run.php
+++ tests/run.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
-ini_set('log_errors', __DIR__.'/../php.errors');
+ini_set('log_errors', __DIR__.'/php.errors');
 
 require_once __DIR__.'/../vendor/autoload.php';
 
